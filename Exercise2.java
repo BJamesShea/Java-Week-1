bff2bdacf51955efb16b10402132b5d1d4005a48
Brandon Shea <brandon.shea1994@icloud.com>
2024-09-09 17:18:47 -0230

    Exercise 2

--- Exercise2.java
+++ Exercise2.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Exercise2 {
     public static void main(String[] args) {
-        
+         Scanner input = new Scanner(System.in);
+        int largest = Integer.MIN_VALUE;
+        int value = 0;
+
+        System.out.println("Enter 5 numbers separated by spaces.");
+        for (int i = 0; i <= 4; i++) {
+            value = input.nextInt();
+            if (value > largest) {
+                largest = value;
+            }
+        }
+         System.out.println("The largest nunber is: " + largest);
     }
     
 }
